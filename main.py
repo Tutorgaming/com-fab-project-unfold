cc3c397f7087df7168dce2075705f81d7f419918
Tutorgaming <tutorgaming@gmail.com>
2022-06-02 15:42:05 +0700

    Last Commit

--- main.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from modules.file_manager import read_stl
-
-
-
-if __name__ == "__main__":
-    stl_path = "./stl/cube.stl"
-    vertices, faces = read_stl(stl_path)
-    print (vertices)
